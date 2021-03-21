f7859e5a385169b54b8de781f2194c1f6e29ce37
nikhilgy <nikhilgyanchandani10@gmail.com>
2021-03-22 00:21:21 +0530

    Coding Challenge 3

--- script.js
+++ script.js
@@ -59,3 +59,52 @@ if(!markHigherBMI){
 else{
     console.log(`According to Data 1: John's BMI(${johnBMI}) is higher than Mark's BMI(${marksBMI})`);
 }
+
+
+//********************************************************************************************************** */
+
+//Coding Challenge #3
+
+// There are two gymnastics teams, Dolphins and Koalas. They compete against each
+// other 3 times. The winner with the highest average score wins a trophy!
+// Your tasks:
+// 1. Calculate the average score for each team, using the test data below
+// 2. Compare the team's average scores to determine the winner of the competition,
+// and print it to the console. Don't forget that there can be a draw, so test for that
+// as well (draw means they have the same average score)
+// 3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a
+// team only wins if it has a higher score than the other team, and the same time a
+// score of at least 100 points. Hint: Use a logical operator to test for minimum
+// score, as well as multiple else-if blocks 😉
+// 4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when
+// both teams have the same score and both have a score greater or equal 100
+// points. Otherwise, no team wins the trophy
+// Test data:
+// § Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+// § Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+// § Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+// GOOD LUCK 😀
+
+
+//Solution
+//According to Data 1:
+const koalasAvg = (88 + 91 + 110)/3
+const dolphinsAvg = (96 + 108 + 89)/3
+
+
+
+if(koalasAvg > dolphinsAvg && koalasAvg>=100)
+{
+    console.log(`Koalas(${koalasAvg}) win Trophy over Dolphins(${dolphinsAvg})`);
+}
+else if(dolphinsAvg > koalasAvg && dolphinsAvg >=100){
+    console.log(`Dolphins(${dolphinsAvg}) win Trophy over Koalas(${koalasAvg})`);
+}
+else if(koalasAvg === dolphinsAvg && koalasAvg>=100)
+{
+    console.log(`Its a draw!`);
+}
+else{
+    console.log(`No Team wins the Trophy!`);
+}
+
